fff67d89b86a00211d301341fabdf4247d50e565
Jonas Kvinge <jonas@jkvinge.net>
2023-02-22 13:53:51 +0100

    wrapper: Add configuration to e-mail report

--- scripts/wrapper.sh
+++ scripts/wrapper.sh
@@ -36,6 +36,28 @@ error() {
 
 }
 
+json_array_to_strlist() {
+
+  local i
+  local output
+  count=$(jq -r "${1} | length" "${CONFIGFILE}")
+  for ((i = 0; i < count; i++)); do
+    local value
+    value=$(jq -r "${1}[${i}]" "${CONFIGFILE}" | sed 's/^null$//g')
+    if [ $? -ne 0 ] || [ "$value" = "" ] ; then
+      continue
+    fi
+    if [ "${output}" = "" ]; then
+      output="${value}"
+    else
+      output="${output} ${value}"
+    fi
+  done
+
+  echo "${output}"
+
+}
+
 
 # Init
 
@@ -154,6 +176,218 @@ if [ "${MAILFROM}" = "" ] || [ "${MAILTO}" = "" ]; then
 fi
 
 
+# Read configuration for e-mail report
+
+if [ "${SCRIPT}" = "azdump.sh" ]; then
+
+  # Find the job index for this job ID
+
+  jobs=$(jq -r ".jobs | length" "${CONFIGFILE}")
+  if [ "${jobs}" = "" ] || [ -z "${jobs}" ] || ! [ "${jobs}" -eq "${jobs}" ] 2>/dev/null; then
+    error "Can't read jobs from Json configuration."
+    exit 1
+  fi
+
+  job_idx=
+  for ((i = 0; i < jobs; i++)); do
+    jobid_current=$(jq -r ".jobs[${i}].id" "${CONFIGFILE}" | sed 's/^null$//g')
+    if [ $? -ne 0 ] || [ "${jobid_current}" = "" ]; then
+      continue
+    fi
+    if [ "${jobid_current}" = "${JOBID}" ]; then
+      job_idx="${i}"
+      break
+    fi
+  done
+
+  if [ "${job_idx}" = "" ]; then
+    error "No job ID ${JOBID} in Json configuration."
+    exit 1
+  fi
+
+  crontab=$(jq -r ".jobs[${job_idx}].crontab" "${CONFIGFILE}")
+  debug=$(jq -r ".jobs[${job_idx}].debug" "${CONFIGFILE}")
+
+  bs_count=$(jq -r ".jobs[${job_idx}].blobstorages | length" "${CONFIGFILE}")
+  if [ "${bs_count}" = "" ] || [ -z "${bs_count}" ] || ! [ "${bs_count}" -eq "${bs_count}" ] 2>/dev/null; then
+    bs_count=0
+    error "Can't read blobstorages from Json configuration."
+  fi
+
+  if [ "${bs_count}" -eq 0 ]; then
+    error "No blobstorages for ${JOBID} in Json configuration."
+  fi
+
+  for ((bs_idx = 0; bs_idx < bs_count; bs_idx++)); do
+
+    source=$(jq -r ".jobs[${job_idx}].blobstorages[${bs_idx}].source" "${CONFIGFILE}" | sed 's/^null$//g')
+    if [ $? -ne 0 ] || [ "${source}" = "" ]; then
+      error "Missing source for job index ${job_idx} ID ${JOBID}."
+      result=1
+      continue
+    fi
+
+    destination=$(jq -r ".jobs[${job_idx}].blobstorages[${bs_idx}].destination" "${CONFIGFILE}" | sed 's/^null$//g')
+    if [ $? -ne 0 ] || [ "${destination}" = "" ]; then
+      error "Missing destination for job index ${job_idx} ID ${JOBID}."
+      result=1
+      continue
+    fi
+
+    source_stripped=$(echo "${source}" | cut -d '?' -f 1)
+
+    blobstorage="Source: ${source_stripped}
+Destination: ${destination}"
+
+    if [ "${blobstorages}" = "" ]; then
+      blobstorages="${blobstorage}"
+    else
+      blobstorages="${blobstorages}
+${blobstorage}"
+    fi
+
+  done
+
+  configuration="Crontab entry: ${crontab}
+Debug: ${debug}
+${blobstorages}"
+
+elif [ "${SCRIPT}" = "pgdump.sh" ]; then
+
+  # Find the job index for this job ID
+
+  jobs=$(jq -r ".jobs | length" "${CONFIGFILE}")
+  if [ "${jobs}" = "" ] || [ -z "${jobs}" ] || ! [ "${jobs}" -eq "${jobs}" ] 2>/dev/null; then
+    error "Can't read jobs from Json configuration."
+    exit 1
+  fi
+
+  job_idx=
+  for ((i = 0; i < jobs; i++)); do
+    jobid_current=$(jq -r ".jobs[${i}].id" "${CONFIGFILE}" | sed 's/^null$//g')
+    if [ $? -ne 0 ] || [ "${jobid_current}" = "" ]; then
+      continue
+    fi
+    if [ "${jobid_current}" = "${JOBID}" ]; then
+      job_idx="${i}"
+      break
+    fi
+  done
+
+  if [ "${job_idx}" = "" ]; then
+    error "No job ID ${JOBID} in Json configuration."
+    exit 1
+  fi
+
+  crontab=$(jq -r ".jobs[${job_idx}].crontab" "${CONFIGFILE}")
+  debug=$(jq -r ".jobs[${job_idx}].debug" "${CONFIGFILE}")
+
+  # Iterate servers
+
+  server_count=$(jq -r ".jobs[${job_idx}].servers | length" "${CONFIGFILE}")
+  if [ "${server_count}" = "" ] || [ -z "${server_count}" ] || ! [ "${server_count}" -eq "${server_count}" ] 2>/dev/null; then
+    error "Can't read servers for ${JOBID} from Json configuration."
+    server_count=0
+  fi
+
+  if [ "${server_count}" -eq 0 ]; then
+    error "No servers for ${JOBID} in Json configuration."
+  fi
+
+
+  for ((server_idx = 0; server_idx < server_count; server_idx++)); do
+
+    PGHOST=$(jq -r ".jobs[${job_idx}].servers[${server_idx}].host" "${CONFIGFILE}" | sed 's/^null$//g')
+    if [ $? -ne 0 ] || [ "${PGHOST}" = "" ]; then
+      error "Missing host for server at index ${server_idx} for job ID ${JOBID}."
+      result=1
+      continue
+    fi
+
+    PGPORT=$(jq -r ".jobs[${job_idx}].servers[${server_idx}].port" "${CONFIGFILE}" | sed 's/^null$//g')
+    if [ $? -ne 0 ]; then
+      PGPORT="5432"
+    fi
+
+    PGUSERNAME=$(jq -r ".jobs[${job_idx}].servers[${server_idx}].user" "${CONFIGFILE}" | sed 's/^null$//g')
+    if [ $? -ne 0 ] || [ "${PGUSERNAME}" = "" ]; then
+      error "Missing user for server ${PGHOST}."
+      result=1
+      continue
+    fi
+
+    PGPASSWORD=$(jq -r ".jobs[${job_idx}].servers[${server_idx}].pass" "${CONFIGFILE}" | sed 's/^null$//g')
+    if [ $? -ne 0 ] || [ "${PGPASSWORD}" = "" ]; then
+      error "Missing pass for ${PGHOST}."
+      result=1
+      continue
+    fi
+
+    backuppath=$(jq -r ".jobs[${job_idx}].servers[${server_idx}].backuppath" "${CONFIGFILE}" | sed 's/^null$//g')
+    if [ $? -ne 0 ] || [ "${backuppath}" = "" ]; then
+      error "Missing backuppath for ${PGHOST}."
+      continue
+    fi
+
+    filenamedate=$(jq -r ".jobs[${job_idx}].servers[${server_idx}].filenamedate" "${CONFIGFILE}" | sed 's/^null$//g')
+    compress=$(jq -r ".jobs[${job_idx}].servers[${server_idx}].compress" "${CONFIGFILE}" | sed 's/^null$//g')
+
+    databases=$(jq -r ".jobs[${job_idx}].servers[${server_idx}].databases[] | keys[]" "${CONFIGFILE}" | tr '\n' ' ')
+    databases_included=$(json_array_to_strlist ".jobs[${job_idx}].servers[${server_idx}].databases_included")
+    databases_excluded=$(json_array_to_strlist ".jobs[${job_idx}].servers[${server_idx}].databases_excluded")
+
+    database_configuration=""
+    databases_configuration=""
+
+    for database in ${databases}
+    do
+      tables_included=$(json_array_to_strlist ".jobs[${job_idx}].servers[${server_idx}].databases[0][\"${database}\"].tables_included")
+      tables_excluded=$(json_array_to_strlist ".jobs[${job_idx}].servers[${server_idx}].databases[0][\"${database}\"].tables_excluded")
+      database_configuration="Database: ${database}
+Tables included: ${tables_included}
+Tables excluded: ${tables_excluded}"
+      if [ "${databases_configuration}" = "" ]; then
+        databases_configuration="${database_configuration}"
+      else
+        databases_configuration="${databases_configuration}
+${database_configuration}"
+      fi
+
+    done
+
+    entry_server="Postgres server: ${PGHOST}
+Postgres port: ${PGPORT}
+Postgres username: ${PGUSERNAME}
+Backup path: ${backuppath}
+Filename date: ${filenamedate}
+Compress: ${compress}
+Included databases: ${databases_included}
+Excluded databases: ${databases_excluded}"
+
+    if [ ! "${databases_configuration}" = "" ]; then
+    entry_server="${entry_server}
+Database configuration:
+${databases_configuration}"
+    fi
+
+    if [ "${entry_servers}" = "" ]; then
+      entry_servers="${entry_server}"
+    else
+      entry_servers="${entry_servers}
+${entry_server}"
+    fi
+
+  done
+
+  configuration="Crontab entry: ${crontab}
+Debug: ${debug}
+${entry_servers}"
+
+else
+  error "Unknown script ${SCRIPT}."
+  exit 1
+fi
+
 # Create lockfile and make sure that script is not already running
 
 LOCKFILE="/tmp/LOCKFILE_${SCRIPTFILENAME}_${JOBID}"
@@ -215,12 +449,13 @@ fi
 
 attachments="${attachments} --"
 
-message="
-Vendanor CloudDump report
+message="Vendanor CloudDump report
 
 Backup server: ${BACKUPSERVER}
 Script: ${SCRIPTFILENAME}
 Job ID: ${JOBID}
+${configuration}
+
 Result: ${result_text}
 Started: ${time_start_timestamp}
 Completed: $(timestamp)
