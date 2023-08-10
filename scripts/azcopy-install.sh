e61831317463255e70e1e30d57f7ffd4a447090b
Jonas Kvinge <jonas@jkvinge.net>
2023-08-10 10:21:33 +0200

    azcopy-install: Prefix `/tmp` in commands instead of changing directory
    Also add.: `curl -f` - Fail on HTTP errors.

--- scripts/azcopy-install.sh
+++ scripts/azcopy-install.sh
@@ -6,9 +6,8 @@ azcopy_date="20230420"
 azcopy_filename="azcopy_linux_amd64_${azcopy_version}.tar.gz"
 azcopy_url="https://azcopyvnext.azureedge.net/release${azcopy_date}/${azcopy_filename}"
 
-cd /tmp || exit 1
-curl -O -L "${azcopy_url}" || exit 1
-tar -xf "${azcopy_filename}" || exit 1
-rm "${azcopy_filename}" || exit 1
-mv $(ls -1 azcopy_linux_*/azcopy | tail -1) /usr/bin || exit 1
-rm -rf azcopy_linux_* || exit 1
+curl -f -O -L --output-dir "/tmp" "${azcopy_url}" || exit 1
+tar -C /tmp -xf "/tmp/${azcopy_filename}" || exit 1
+rm "/tmp/${azcopy_filename}" || exit 1
+mv $(ls -1 /tmp/azcopy_linux_*/azcopy | tail -1) /usr/bin || exit 1
+rm -rf /tmp/azcopy_linux_* || exit 1
