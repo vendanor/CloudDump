152abfd07a050945684e72c3f31030562725180a
ralftar <ralf.taraldset@rbt.no>
2025-03-06 12:53:52 +0000

    Update azcopy-install.sh

--- scripts/azcopy-install.sh
+++ scripts/azcopy-install.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-azcopy_url="https://azcopyvnext-awgzd8g7aagqhzhe.b02.azurefd.net/releases/release-10.28.0-20250127/azcopy_linux_amd64_10.28.0.tar.gz"
+set -e  # Exit immediately if a command fails
 
-curl -f -O -L --output-dir "/tmp" "${azcopy_url}" || exit 1
-tar -C /tmp -xf "/tmp/${azcopy_filename}" || exit 1
-rm "/tmp/${azcopy_filename}" || exit 1
-mv $(ls -1 /tmp/azcopy_linux_*/azcopy | tail -1) /usr/bin || exit 1
-rm -rf /tmp/azcopy_linux_* || exit 1
+azcopy_url="https://azcopyvnext-awgzd8g7aagqhzhe.b02.azurefd.net/releases/release-10.28.0-20250127/azcopy_linux_amd64_10.28.0.tar.gz"
+azcopy_filename="azcopy.tar.gz"
+
+# Download AzCopy
+curl -f -L -o "/tmp/${azcopy_filename}" "${azcopy_url}"
+
+# Extract
+tar -C /tmp -xf "/tmp/${azcopy_filename}"
+rm "/tmp/${azcopy_filename}"
+
+# Move AzCopy binary to /usr/bin
+mv /tmp/azcopy_linux_*/azcopy /usr/bin/azcopy
+
+# Cleanup
+rm -rf /tmp/azcopy_linux_*
