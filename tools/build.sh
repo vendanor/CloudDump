e4005562f64c4cd05da1f24672d09cbf422b0787
ralftar <ralf.taraldset@rbt.no>
2025-03-06 12:26:01 +0000

    remove debian, fedora and ubuntu

--- tools/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-distro="$1"
-if [ "${distro}" = "" ]; then
-  distro="ubuntu"
-fi
-
-sudo docker-compose build clouddump-${distro}
--- tools/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo docker-compose down
-sudo docker rm $(sudo docker ps -aq)
-sudo docker rmi $(sudo docker images -q)
-sudo docker volume rm $(sudo docker volume ls -qf dangling=true)
